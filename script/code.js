876c8bf93007887b6e720fb4b118db23206294c7
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-05-17 15:12:13 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,21 @@
+let person = {
+    firstName: "Que",
+    lastName: "Mnqika",
+    age: 23,
+    favColor: "Blue",
+}
+
+let person2 = {
+    firstName: "Jillet",
+    lastName: "Hope",
+    age: 16,
+    favColor: "Pink",
+}
+
+let arr = []
+
+let added = arr.push(person, person2)
+
+console.log(person);
+console.log(person2);
+console.log(arr);
